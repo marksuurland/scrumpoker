6266ac315a7341f3c64861f64f40be9b0db21b00
Mark Suurland <msuurland@businessforensics.nl>
2020-05-10 21:17:40 +0200

    function to add a new pokergame

--- functions/src/pokergame.ts
+++ functions/src/pokergame.ts
@@ -7,11 +7,9 @@ exports.addPokerRoom = functions
     .region('europe-west1')
     .https.onCall(async (data, context) => {
         const name = data.name;
-        // Push the new message into the Realtime Database using the Firebase Admin SDK.
         const newItem = await admin.firestore().collection('/items').add({
                 name,
                 players: []
         });
-        console.log('newItem', newItem);
-        return { test: 'test'};
+        return { id: newItem.id};
     });
